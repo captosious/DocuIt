6697b040c78a5dd2a476916de3c7c9c5f1197924
codeforgery <roc.rio@icloud.com>
2020-09-30 16:26:14 +0200

    Hola

--- DocuitWeb/Resources/Components/BuildingType.ca-ES.Designer.cs
+++ DocuitWeb/Resources/Components/BuildingType.ca-ES.Designer.cs
@@ -16,21 +16,21 @@ namespace DocuitWeb.Resources.Components {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class WorkingCenter_es_ES {
+    internal class BuildingType_ca_ES {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal WorkingCenter_es_ES() {
+        internal BuildingType_ca_ES() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("DocuitWeb.Resources.Components.WorkingCenter.es-ES", typeof(WorkingCenter_es_ES).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("DocuitWeb.Resources.Components.BuildingType.ca-ES", typeof(BuildingType_ca_ES).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
