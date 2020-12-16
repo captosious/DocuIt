5e10d572f69dfadf969d55e43a6b485ce9605527
codeforgery <roc.rio@icloud.com>
2020-12-16 17:21:24 +0100

    UP AND GO

---
+++ docuit/Controllers/InventoryController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DocuItService.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DocuItService.Controllers
+{
+    public class InventoryController : ControllerBase
+    {
+        private readonly DocuItContext MyDBContext;
+        private readonly MyAppSettings MySettings;
+
+        public InventoryController(DocuItContext db, MyAppSettings MySettings)
+        {
+            MyDBContext = db;
+            this.MySettings = MySettings;
+        }
+
+        [HttpGet("GetInventory")]
+        public IEnumerable<InventoryReport> GetInventory([FromBody] InventoryReport params)
+        {
+            IEnumerable<InventoryReport> 
+            return 
+        }
+    }
+}
