1b1bb6b4f9c2c1a05fc2cca4d4f25445824f8ff4
codeforgery <roc.rio@icloud.com>
2021-01-19 16:39:19 +0100

    fff

---
+++ docuit/Models/QuestionnaireParams.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DocuItService.Models
+{
+    public partial class QuestionnaireParams
+    {
+        public int CompanyId { get; set; }
+        public string QuestionnaireTypeId { get; set; }
+    }
+}
